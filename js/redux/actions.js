268891e022445dc67ac4923bc1016e81ea7c1d60
Igor.Mikhailov <iomihailov@gmail.com>
2021-02-11 17:11:22 +0300

    added logic of deleting products from server

--- js/redux/actions.js
+++ js/redux/actions.js
@@ -2,11 +2,12 @@ import {DELETE_PRODUCT, SET_PRODUCTS} from './types.js';
 import API from '../api/products.js';
 import {dispatch} from "./index.js";
 
-export const deleteProduct = (id) => ({type: DELETE_PRODUCT, payload: id});
+export const deleteProduct = (id) =>
+  API.delProduct(id)
+    .then( () => dispatch(({type: DELETE_PRODUCT, payload: id})))
+    .catch(err => console.log(err))
 
 export const getProducts = () =>
-  API.getProducts().then(
-    (products) => dispatch({type: SET_PRODUCTS, payload: products}),
-    () => alert('Ooops, something went wrong')
-  );
-  
+  API.getProducts()
+    .then((products) => dispatch({type: SET_PRODUCTS, payload: products}))
+    .catch(err => console.log(err))
